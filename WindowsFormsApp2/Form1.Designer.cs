a3a62e97355db7e487c85b17fb20f12ab4d07767
minzixiaoshile <48387476+minzixiaoshile@users.noreply.github.com>
2019-11-17 19:59:16 +0800

    em

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "软盘君";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(141, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 70);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Mr Chen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(270, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 70);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "FinderMister";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.Location = new System.Drawing.Point(399, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(123, 69);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "TheColdWinte寒冬";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button5.Location = new System.Drawing.Point(12, 88);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(123, 69);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "依寒";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button6.Location = new System.Drawing.Point(270, 88);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(252, 69);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "萌橙";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button7.Location = new System.Drawing.Point(12, 163);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(123, 69);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "犬吠九天";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button8.Location = new System.Drawing.Point(141, 163);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(123, 69);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "新地平线";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button9.Location = new System.Drawing.Point(270, 163);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(249, 69);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "酸菜鱼";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button10.Location = new System.Drawing.Point(141, 88);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(123, 69);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "VmLu_Rui";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button13);
+            this.groupBox1.Controls.Add(this.button12);
+            this.groupBox1.Controls.Add(this.button11);
+            this.groupBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 238);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(507, 105);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SB-FinderMister团队的“名人名言”";
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(264, 25);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(123, 69);
+            this.button13.TabIndex = 2;
+            this.button13.Text = "TheColdWinte寒冬";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(135, 25);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(123, 69);
+            this.button12.TabIndex = 1;
+            this.button12.Text = "FinderMister";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button11.Location = new System.Drawing.Point(6, 25);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(123, 69);
+            this.button11.TabIndex = 0;
+            this.button11.Text = "软盘君";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 351);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "Form1";
+            this.Text = "名字已消失了认识的人的评价C#版";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("一个背叛者，并且还会爆破服务器端口", "软盘君", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("依寒分身体", "Mr Chen",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            MessageBox.Show("依寒分身体", "Mr Chen", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            MessageBox.Show("依寒分身体", "Mr Chen", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            MessageBox.Show("重要的事情需要说三遍", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("一个沙雕杠精", "FinderMister", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("一个不懂版权的（营销号行为），就比如“转载的视频剪辑过就可以投自制”", "TheColdWinte寒冬",MessageBoxButtons.OK,MessageBoxIcon.Stop);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("是个戏精", "依寒", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            MessageBox.Show("是个戏精", "依寒", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            MessageBox.Show("是个戏精", "依寒", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            MessageBox.Show("重要的事情需要说三遍", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("萌橙就是依寒", "萌橙", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            MessageBox.Show("萌橙就是依寒", "萌橙", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            MessageBox.Show("萌橙就是依寒", "萌橙", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            MessageBox.Show("重要的事情需要说三遍", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("是个很皮的人", "狗吠九天");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("诸葛亮杀毒作者", "新地平线");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("会c++的", "酸菜鱼");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("会威胁别人", "VmLu_Rui", MessageBoxButtons.OK,MessageBoxIcon.Warning);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("老有意思——刘永康\n你觉得有人愿意和rz和平？——刘永康","软盘君",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("砸蔡**——FinderMister\n转载的视频剪辑后投自制——FinderMister\n名字已消失了他妈的名字叫赖艳红——FinderMister\n爷笑了——FinderMister\n爷——FinderMister\n三天之内撒了你——FinderMister\n给爷爬——FinderMister", "FinderMister", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("转载的视频剪辑过就可以投自制——TheColdWinte寒冬\n砸蔡**——TheColdWinte寒冬", "TheColdWinte寒冬", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
